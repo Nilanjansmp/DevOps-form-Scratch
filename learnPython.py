9b5af885567c03844a8662d6c1bc2a1631b667c8
nilanjan <nilanjan11998@gmail.com>
2023-12-11 23:16:14 +0530

    variable and data type

--- learnPython.py
+++ learnPython.py
@@ -1,5 +1,19 @@
+#learn about variable how does it stores a value
 a=10
+print("Hello Python ______ :)")
 b=15
-print(a+b)
-print(a-b)
-print(a*b)
+"""
+print the value of addition , multiplication and substraction of three numbers
+
+"""
+s = a+b
+sub = a-b
+mul = a*b
+print("sum of two number is  = ",s)
+print("substraction of two number is = ",sub)
+print("Multiple of two number is = ",mul)
+x=2.5
+y=300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+z='nilanjan'
+m=True
+print("type of x is ",type(x),"type of y is",type(y),"type of z is ",type(z),"type of m is ",type(m),sep="              ")
